c097069c6614bdfe3a849f51e54488145502c39a
andrei15021991 <50627333+andrei15021991@users.noreply.github.com>
2020-11-09 16:53:00 +0300

    Add files via upload

---
+++ travel-agency-servlet-jsp/src/main/java/com/andersen_trainee/travel_agency_servlet/dao/cp/ConnectionPoolTool.java
@@ -0,0 +1,13 @@
+package com.andersen_trainee.travel_agency_servlet.dao.cp;
+
+public final class ConnectionPoolTool {
+	public static final String URL = "jdbc:mysql://127.0.0.1/andresen_trainee?characterEncoding=utf8&serverTimezone=Europe/Minsk&useSSL=false";
+	public static final String USER = "root";
+	public static final String PASSWORD = "123456";
+	public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+
+	private ConnectionPoolTool() {
+
+	}
+
+}
